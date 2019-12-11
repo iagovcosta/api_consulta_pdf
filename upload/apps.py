ff8eac2f1f1837a275a5982d92651e0000c78732
iagocosta <iagovianna1@gmail.com>
2019-12-11 13:09:45 -0300

    big bang

---
+++ upload/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RecebePdfConfig(AppConfig):
+    name = 'upload'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/iagocosta/Documents/pesquisa_pdf/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
